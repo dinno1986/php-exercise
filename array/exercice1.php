987c29b200b0205f72c467506de9f9478530f203
dinno1986 <dimitri.castell@hotmail.fr>
2017-06-21 16:51:12 +0200

    ajout exercice 1 et 2

---
+++ array/exercice1.php
@@ -0,0 +1,11 @@
+<?php
+
+$date = array(
+	'Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre',
+	);
+
+echo '<pre>';
+print_r($date);
+echo '</pre>';
+
+?>
