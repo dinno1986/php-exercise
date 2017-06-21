81b8da03c16649b38466fecd5d3e8812ebec8587
dinno1986 <dimitri.castell@hotmail.fr>
2017-06-21 17:18:54 +0200

    ajout des exos 3 et 4

---
+++ array/exercice3.php
@@ -0,0 +1,8 @@
+<?php
+
+$date = array(
+	'Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre',
+	);
+echo $date[5];
+
+?>
---
+++ array/exercice4.php
@@ -0,0 +1,15 @@
+<?php
+
+$date = array(
+	'Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Décembre',
+	);
+
+$date[7] = 'Août';
+echo $date[7];
+?>
+
+
+
+
+
+
