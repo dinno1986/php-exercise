bc0d5cce2ef78ab2b9635c86a01af8b5d0a3fa7f
dinno1986 <dimitri.castell@hotmail.fr>
2017-06-22 12:08:36 +0200

    fin des exercices paramURL

--- paramUrl/index.php
+++ paramUrl/index.php
@@ -1,16 +1,39 @@
 <?php
-//exo1
-$nom=$_GET['nom'];
-$prenom=$_GET['prenom'];
-$age=$_GET['age'];
-echo($nom." ".$prenom." ");
+// //exo1
+// $nom=$_GET['nom'];
+// $prenom=$_GET['prenom'];
+// $age=$_GET['age'];
+// echo($nom." ".$prenom." ");
 
 //exo2
-if($age){
-	echo "$age"." ".ans ;
-}
-else{
-	echo "error";
-}
+// if($age=""){
+// 	echo "$age"." ".ans ;
+// }
+// else{
+// 	echo "error";
+// }
+
+//exo3
+
+// $dateDebut=$_GET['dateDebut'];
+// $dateFin=$_GET['dateFin'];
+// echo($dateDebut." / ".$dateFin);
+
+//exo4
+
+// $langage=$_GET['langage'];
+// $serveur=$_GET['serveur'];
+// echo($langage." ".$serveur);
+
+//exo5
+
+// $semaine=$_GET['semaine'];
+// echo($semaine);
+
+//exo6
+
+$batiment=$_GET['batiment'];
+$salle=$_GET['salle'];
+echo ("batiment".$batiment." "."salle".$salle);
  
 ?>
