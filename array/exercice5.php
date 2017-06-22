2ce8377efb6beca5b3e1393e48a36d9b0789a345
dinno1986 <dimitri.castell@hotmail.fr>
2017-06-22 10:16:30 +0200

    ajout des exercices 5,6,7

---
+++ array/exercice5.php
@@ -0,0 +1,13 @@
+<?php
+
+$region = array (
+	'02' => 'Aisne',
+	'59' => 'Nord',
+	'60' => 'Oise',
+	'62' => 'Pas de Calais',
+	'80' => 'Somme',
+	);
+
+echo $region;
+
+?>
---
+++ array/exercice7.php
@@ -0,0 +1,15 @@
+<?php
+
+$region = array (
+	'02' => 'Aisne',
+	'59' => 'Nord',
+	'60' => 'Oise',
+	'62' => 'Pas de Calais',
+	'80' => 'Somme',
+	'51' => 'Marne',
+
+	);
+
+
+
+?>
