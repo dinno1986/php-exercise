3d1db424b113defc27f51605c6b001ba9a1fb9d0
dinno1986 <dimitri.castell@hotmail.fr>
2017-06-22 11:39:53 +0200

    Ajout des exercices 1 et 2

---
+++ paramUrl/index.php
@@ -0,0 +1,16 @@
+<?php
+//exo1
+$nom=$_GET['nom'];
+$prenom=$_GET['prenom'];
+$age=$_GET['age'];
+echo($nom." ".$prenom." ");
+
+//exo2
+if($age){
+	echo "$age"." ".ans ;
+}
+else{
+	echo "error";
+}
+ 
+?>
