04d494335fac24aedd20f04b909605813a7714c1
dinno1986 <dimitri.castell@hotmail.fr>
2017-06-23 16:10:24 +0200

    rajout modif des cookies

--- globalVariable/exo3/recup.php
+++ globalVariable/exo3/recup.php
@@ -4,6 +4,10 @@
 //affiche les cookies
   $cookie1=$_COOKIE['login'];
   $cookie2=$_COOKIE['mdp'];
-  echo "login :"." ".$cookie1." "."mot de passe :".$cookie2
+  echo "login :"." ".$cookie1." "."mot de passe :".$cookie2;
+
+//modifier les cookies
+   setcookie('login', 'Dim', time() + 365*24*3600, null, null, false, true);
+
 
 ?>
