8af393469ac68293b91f9311388ac339145e84ed
dinno1986 <dimitri.castell@hotmail.fr>
2017-06-23 12:15:39 +0200

    ajout de l'exercice 7 et 8

---
+++ form/exo7/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<form method="POST" action="index.php" enctype="multipart/form-data">
+
+		<select name="genre">
+			<option value="Mr">Mr</option>
+			<option value="Mme">Mme</option>
+		</select>
+
+		<label for="nom">Nom</label>
+		<input id="nom" type="text" name="nom" />
+
+		<label for="prenom">Prénom</label>
+		<input type="text" name="prenom" />
+
+		<label for="fichier">fichier</label>
+		<input type="file" name="fichier" />
+
+		<button name="button" type="submit" value="OK">OK</button>
+
+		<?php
+
+
+if (isset($_FILES[fichier])) {
+
+	$extensions_valides = array( 'pdf' ,  );
+	//1. strrchr renvoie l'extension avec le point (« . »).
+	//2. substr(chaine,1) ignore le premier caractère de chaine.
+	//3. strtolower met l'extension en minuscules.
+	$extension_upload = strtolower(  substr(  strrchr($_FILES['fichier']['name'], '.')  ,1)  );
+	if ( in_array($extension_upload,$extensions_valides) ) echo " Envoi du fichier Extension correcte";
+	else{
+		echo" le fichier doit être au format pdf";
+	}
+}
+
+	
+
+
+	
+ // 	$genre=$_POST['genre'];
+	// $prenom=$_POST['prenom'];
+	// $nom=$_POST['nom'];
+	// $fichier=$_POST['fichier'];
+	// echo($genre." ".$prenom." ".$nom." ".$fichier);   
+	?>
+
+	</form>
+
+</body>
+</html>
+
+
